abc949c670929adcbbcf3f4ed85c53b1a97e0f9f
Zaghluol <alfrdwsj49@gmail.com>
2026-03-04 01:03:14 +0200

    AddSearchEndpoint

---
+++ E-commarce_Backend/Dtos/ProductSearchDto.cs
@@ -0,0 +1,17 @@
+﻿namespace E_commarce_Backend.Dtos
+{
+    public class ProductSearchDto
+    {
+        public string? Name { get; set; }       
+        public string? Category { get; set; }      
+
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+
+        public string? SortBy { get; set; }
+        public string? SortDirection { get; set; } = "asc";
+
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
